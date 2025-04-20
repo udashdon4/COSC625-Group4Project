430af57c90a7f1e465af73cf0fda4fe4b6346314
Danny Calise <danny.calise@us.kbr.com>
2025-04-20 17:58:23 -0400

    changed route urls

--- nodejsserver/index.js
+++ nodejsserver/index.js
@@ -11,7 +11,7 @@ const db = require('./db');
 
 // Middleware to enable CORS
 app.use(cors({
-  origin: "http://localhost:3000",  
+  origin: "http://npe-backend.onrender.com:3000",  
   credentials: true                
 }));
 app.use(cors({
