5002e796f91625dac87da576c5616d30fdb803d4
Danny Calise <danny.calise@us.kbr.com>
2025-04-20 19:51:06 -0400

    updated explore href

--- npe-ui/src/pages/LandingPage.js
+++ npe-ui/src/pages/LandingPage.js
@@ -45,7 +45,7 @@ const LandingPage = () => {
         <div className="z-10 max-w-2xl px-4">
           <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold mb-4 leading-tight">Discover the Wonders of Nature.</h1>
           <a 
-            href="COSC625-Group4Project/parksearch" 
+            href="parksearch" 
             className="inline-block mt-8 px-8 py-3 bg-white text-gray-800 font-semibold rounded-md hover:bg-gray-100 transition-colors duration-300 shadow-lg"
           >
             Explore
