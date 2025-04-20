b517e9fa459ab13a11007be7881148f6ff8d7523
Danny Calise <danny.calise@us.kbr.com>
2025-04-20 17:51:42 -0400

    added cors to render backend

--- nodejsserver/index.js
+++ nodejsserver/index.js
@@ -14,6 +14,10 @@ app.use(cors({
   origin: "http://localhost:3000",  
   credentials: true                
 }));
+app.use(cors({
+  origin: 'https://hollywoodchase.github.io',
+  credentials: true
+}));
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Credentials", "true");
   next();
